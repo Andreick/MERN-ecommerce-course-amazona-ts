d1c5128cc2e7a2fc33404d62aed9d202b101817c
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-31 15:33:22 -0300

    Add products screen with sample data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import sampleData from './sampleData';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {sampleData.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
