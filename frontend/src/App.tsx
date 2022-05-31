fa476abd9ebd5d9ce92e6556016c3493370290ef
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-31 15:54:23 -0300

    Add routes with React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,25 @@
-import sampleData from './sampleData';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {sampleData.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/products/${product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
-              </a>
-              <div className="product-info">
-                <a href={`/products/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+            <Route
+              path="/products/:slug"
+              element={<ProductListScreen />}
+            ></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import sampleData from '../sampleData';
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {sampleData.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/products/${product.slug}`}>
+              <img src={product.image} alt={product.name}></img>
+            </Link>
+            <div className="product-info">
+              <Link to={`/products/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
